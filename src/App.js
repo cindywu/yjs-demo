b69a0de5030ace3f29c8aa9d76c45c60f36ddd30
cindy <ciindy.wu@gmail.com>
2021-04-14 12:12:22 -1000

    somewhat working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+import React, { useEffect } from 'react'
 import Quill from 'quill'
+import * as Y from 'yjs'
+// import { QuillBinding } from 'y-quill'
+import QuillCursors from 'quill-cursors'
 
 function App() {
-  var quill = new Quill(document.querySelector('#root'), {
-    modules: {
-      // cursors: true,
-      toolbar: [
-        [{ header: [1, 2, false]}],
-        ['bold', 'italic', 'underline'],
-        ['image', 'code-block']
-      ],
-      history: {
-        // Local undo shouldn't undo changes
-        // from remote users
-        userOnly: true
-      }
-    },
-    placeholder: 'Start collaborating...',
-    theme: 'snow' 
-  })
-  
+  useEffect(() => {
+    Quill.register('modules/cursors', QuillCursors)
+
+    const quill = new Quill(document.querySelector('#root'), {
+      modules: {
+        cursors: true,
+        toolbar: [
+          [{ header: [1, 2, false]}],
+          ['bold', 'italic', 'underline'],
+          ['image', 'code-block']
+        ],
+        history: {
+          // Local undo shouldn't undo changes
+          // from remote users
+          userOnly: true
+        }
+      },
+      placeholder: 'Start collaborating...',
+      theme: 'snow' 
+    })
+
+    // A Yjs document holds the shared data
+    const ydoc = new Y.Doc()
+
+    // Define a shared text type on the document
+    const ytext = ydoc.getText(quill)
+
+    // Create an editor-binding which
+    // "binds" the quill editor to a Y.Text type.
+    // const binding = new QuillBinding(quill, ytext)
+  }, [])
+
   return (
     <div>
 
